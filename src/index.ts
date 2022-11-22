1a9df22fc78b61e60c0f5507b3ebe3a3397e39ca
Guillaume Galy <ggaly@gatling.io>
2022-11-22 14:47:45 +0100

    chore: Bootstrap basic GH action project with Typescript
    ref: RND-3
    
    - Typescript config
    - Test config with Jest
    - Packaging with ncc
    - Code sample

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as core from '@actions/core'
+
+const run = ():void => {
+    core.info("Gatling Enterprise GitHub Action (not implemented)")
+    core.setFailed("Not implemented!")
+}
+
+run()
