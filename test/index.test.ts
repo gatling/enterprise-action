11afd09a2260aa4a8f2d6c1abea200c3bf7d2e72
Guillaume Galy <ggaly@gatling.io>
2022-11-22 16:37:51 +0100

    chore: Configure Prettier
    ref: RND-3

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
-import {expect, test} from '@jest/globals'
+import { expect, test } from "@jest/globals";
 
-test('Example test', () => {
-    expect(1 + 1 === 2).toBe(true)
-    expect(1 + 1 === 3).toBe(false)
-})
+test("Example test", () => {
+  expect(1 + 1 === 2).toBe(true);
+  expect(1 + 1 === 3).toBe(false);
+});
