f3c9efe0cffc5a29e0ccb9f9243b1f6d99e9ec28
Guillaume Galy <ggaly@gatling.io>
2022-11-22 15:41:11 +0100

    chore: Add inputs and outputs to sample code
    ref: RND-3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import * as core from "@actions/core";
+import { wait } from "./wait";
 
-const run = (): void => {
-  core.info("Gatling Enterprise GitHub Action (not implemented)");
-  core.setFailed("Not implemented!");
-};
+async function run(): Promise<void> {
+  try {
+    const ms: string = core.getInput("milliseconds");
+    core.debug(`Input milliseconds=${ms}`);
+
+    const fail: string = core.getInput("fail");
+    core.debug(`Input fail=${ms}`);
+
+    await wait(parseInt(ms, 10));
+
+    if (fail === "true") {
+      core.setFailed("The action failed.");
+    } else {
+      core.setOutput("result", "The action succeded");
+    }
+  } catch (error) {
+    const msg = error instanceof Error ? error.message : `Unknown error: ${error}`;
+    core.setFailed(msg);
+  }
+}
 
 run();
